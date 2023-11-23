890d1f7014b6ba113be25d43a4959cdb498c072b
BhabhaSeptember <septemberbhabha@gmail.com>
2023-11-23 07:57:13 +0200

    ch1 ex

--- Chapter 1.sql
+++ Chapter 1.sql
@@ -20,5 +20,36 @@ VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
 
 
 
+-- Practice Exercises
+
+CREATE TABLE all_animals (
+	_id bigserial,
+	nickname varchar(25),
+	animal_type varchar(50)
+);
+
+INSERT INTO all_animals (nickname, animal_type)
+VALUES ('Pipa', 'Hippo'),
+	   ('Terry', 'Tiger'),
+	   ('Ronny', 'Rhino');
+
+
+CREATE TABLE animal_specifics (
+	_id bigserial,
+	nickname varchar(25),
+	species varchar(75),
+	animal_type  varchar(50),
+	arrival_date date,
+	life_expectancy numeric
+);
+
+INSERT INTO animal_specifics(nickname,species, animal_type, arrival_date, life_expectancy)
+VALUES ('Pipa', 'Pygmy hippopotamus', 'Hippo', '1999-07-12', 36),
+	   ('Terry', 'Panthera tigris', 'Tiger', '1995-07-15', 15),
+	   ('Ronny', 'Rhinocerotidae', 'Rhino', '2003-08-22', 45);
+
+
+
+
 
 
