b998966b9ff8fc9f8ee7ee781e8b9798f5187a16
BhabhaSeptember <septemberbhabha@gmail.com>
2023-11-23 08:08:18 +0200

    ch2 exc

--- Chapter 2.sql
+++ Chapter 2.sql
@@ -80,8 +80,29 @@
 --       AND (salary < 38000 OR salary > 40000);
 
 
-SELECT first_name, last_name, school, hire_date, salary
+-- SELECT first_name, last_name, school, hire_date, salary
+-- FROM teachers
+-- WHERE school LIKE '%Roos%'
+-- ORDER BY hire_date DESC;
+
+
+
+
+-- PRACTICE EXERCISES
+
+-- SELECT school, last_name
+-- FROM teachers
+-- ORDER BY school, last_name ASC;
+
+
+-- SELECT first_name, salary
+-- FROM teachers
+-- WHERE first_name ILIKE 'S%'
+-- 	  AND salary > 40000;
+
+
+SELECT *
 FROM teachers
-WHERE school LIKE '%Roos%'
-ORDER BY hire_date DESC;
+WHERE hire_date >= '2010-01-01' 
+ORDER BY salary DESC;
 
