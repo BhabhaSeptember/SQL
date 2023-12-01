3f431625aa189a2a99224fb99834740968c11e14
BhabhaSeptember <septemberbhabha@gmail.com>
2023-12-01 23:08:10 +0200

    ch6 tbc

---
+++ Chapter 6.sql
@@ -0,0 +1,42 @@
+-- Linking Tables Using JOIN
+-- general syntax below
+SELECT * 
+FROM table_a JOIN table_b
+ON table_a.key_column = table_b.foreign_key_column
+
+-- Or match values using a '>=' expression
+ON table_a.key_column >= table_b.foreign_key_column
+
+
+
+-- Relating Tables with Key Columns
+CREATE TABLE departments (
+    dept_id bigserial,
+    dept varchar(100),
+    city varchar(100),
+    CONSTRAINT dept_key PRIMARY KEY (dept_id),
+    CONSTRAINT dept_city_unique UNIQUE (dept, city)
+);
+INSERT INTO departments (dept, city)
+VALUES
+    ('Tax', 'Atlanta'),
+    ('IT', 'Boston');
+
+
+CREATE TABLE employees (
+    emp_id bigserial,
+    first_name varchar(100),
+    last_name varchar(100),
+    salary integer,
+    dept_id integer REFERENCES departments (dept_id),
+    CONSTRAINT emp_key PRIMARY KEY (emp_id),
+    CONSTRAINT emp_dept_unique UNIQUE (emp_id, dept_id)
+);
+INSERT INTO employees (first_name, last_name, salary, dept_id)
+VALUES
+    ('Nancy', 'Jones', 62500, 1),
+    ('Lee', 'Smith', 59300, 1),
+    ('Soo', 'Nguyen', 83000, 2),
+    ('Janet', 'King', 95000, 2);
+
+
